a3e3c9dafc0582203730345f151288c167cad5a4
dankb <dankb@LAPTOP-2J6L4TTB>
2020-09-20 18:23:18 -0500

    - In memory compilation is now working.

--- src/main/java/com/github/sdankbar/jrungen/Main.java
+++ src/main/java/com/github/sdankbar/jrungen/Main.java
@@ -1,14 +1,16 @@
 package com.github.sdankbar.jrungen;
 
+import java.io.IOException;
+
 public class Main {
-	public static void main(final String[] args) {
+	public static void main(final String[] args) throws IOException {
 		final RuntimeCompiler c = new RuntimeCompiler();
-		c.compile(getSource());
+		c.compile("Test", getSource());
 	}
 
 	private static String getSource() {
 		return "public class Test implements Runnable{" + //
 				"public void run(){" + //
-				"System.out.println(\"testing\");}}";
+				"System.out.println(\"Hello World\");}}";
 	}
 }
--- src/main/java/com/github/sdankbar/jrungen/RuntimeCompiler.java
+++ src/main/java/com/github/sdankbar/jrungen/RuntimeCompiler.java
@@ -15,36 +15,71 @@ public class RuntimeCompiler {
 
 	private final JavaCompiler compilerReference = javax.tools.ToolProvider.getSystemJavaCompiler();
 
-	public void compile(final String sourceCode) {
+	public void compile(final String className, final String sourceCode) {
 		final DiagnosticCollector<JavaFileObject> collector = new DiagnosticCollector<>();
 
-		final InmemoryClassFile classOuput = new InmemoryClassFile("Test");
-
-		final StandardJavaFileManager standardManager = compilerReference.getStandardFileManager(collector, null, null);
-		final JavaFileManager wrappedManager = createFileManager(standardManager, classOuput);
+		final InmemoryClassFile classOuput = new InmemoryClassFile(className);
+		final JavaFileManager wrappedManager = createFileManager(collector, classOuput);
 
 		final JavaCompiler.CompilationTask task = compilerReference.getTask(null, wrappedManager, collector, null, null,
-				getCompilationUnits(sourceCode));
+				getCompilationUnits(className, sourceCode));
 
+		final long s = System.currentTimeMillis();
 		if (!task.call()) {
 			collector.getDiagnostics().forEach(System.out::println);
 		}
+		final long e = System.currentTimeMillis();
+		System.out.println("Compiling took " + (e - s) + " milliseconds");
+
+		// loading and using our compiled class
+		final ClassLoader inMemoryClassLoader = createClassLoader(classOuput);
+		Class<Runnable> test;
+		try {
+			test = (Class<Runnable>) inMemoryClassLoader.loadClass(className);
+			final Runnable iTest = test.newInstance();
+
+			wrappedManager.close();
+
+			iTest.run();
+		} catch (final ClassNotFoundException excp) {
+			excp.printStackTrace();
+		} catch (final InstantiationException excp) {
+			excp.printStackTrace();
+		} catch (final IllegalAccessException excp) {
+			excp.printStackTrace();
+		} catch (final IOException excp) {
+			excp.printStackTrace();
+		}
 	}
 
-	private static Iterable<? extends JavaFileObject> getCompilationUnits(final String sourceCode) {
-		final InmemorySourceFile stringObject = new InmemorySourceFile("Test", sourceCode);
-		return Arrays.asList(stringObject);
-	}
-
-	private static JavaFileManager createFileManager(final StandardJavaFileManager fileManager,
+	private JavaFileManager createFileManager(final DiagnosticCollector<JavaFileObject> collector,
 			final InmemoryClassFile byteObject) {
-		return new ForwardingJavaFileManager<StandardJavaFileManager>(fileManager) {
+		final StandardJavaFileManager standardFileManager = compilerReference.getStandardFileManager(collector, null,
+				null);
+
+		return new ForwardingJavaFileManager<StandardJavaFileManager>(standardFileManager) {
 			@Override
 			public JavaFileObject getJavaFileForOutput(final Location location, final String className,
 					final JavaFileObject.Kind kind, final FileObject sibling) throws IOException {
 				return byteObject;
 			}
 		};
+	}
+
+	private static ClassLoader createClassLoader(final InmemoryClassFile byteObject) {
+		return new ClassLoader() {
+			@Override
+			public Class<?> findClass(final String name) throws ClassNotFoundException {
+				final byte[] bytes = byteObject.getBytes();
+				return defineClass(name, bytes, 0, bytes.length);
+			}
+		};
+	}
+
+	public static Iterable<? extends JavaFileObject> getCompilationUnits(final String className,
+			final String sourceCode) {
+		final InmemorySourceFile stringObject = new InmemorySourceFile(className, sourceCode);
+		return Arrays.asList(stringObject);
 	}
 
 }
