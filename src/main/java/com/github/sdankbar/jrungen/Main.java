3b42117eaf9d1a350cbc161150471ced9ad9d23e
dankb <dankb@LAPTOP-2J6L4TTB>
2020-09-20 19:26:36 -0500

    - Added a method to get a Function interface back for a body of code.

--- src/main/java/com/github/sdankbar/jrungen/Main.java
+++ src/main/java/com/github/sdankbar/jrungen/Main.java
@@ -1,19 +1,44 @@
 package com.github.sdankbar.jrungen;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.function.Function;
+
 public class Main {
 	public static void main(final String[] args)
-			throws CompilationException, InstantiationException, IllegalAccessException {
+			throws CompilationException, InstantiationException, IllegalAccessException, NoSuchMethodException,
+			SecurityException, IllegalArgumentException, InvocationTargetException {
 		final RuntimeCompiler c = new RuntimeCompiler();
-		final Class<Runnable> classObj = c.compile("Test", getSource());
+
+		testBasicCompile(c);
+
+		testFunctionalInterface(c);
+	}
+
+	private static void testFunctionalInterface(final RuntimeCompiler c) throws CompilationException {
+		final Class<Function<String, Integer>> classObj = c.compileFunctionalInterface(String.class, Integer.class,
+				"return Integer.valueOf(arg, 16);");
+
+		final Function<String, Integer> f = RuntimeCompiler.constructInstance(classObj);
+		System.out.print("Converted \"F\" to " + f.apply("F"));
+	}
+
+	private static void testBasicCompile(final RuntimeCompiler c) throws CompilationException {
+		Class<Runnable> classObj = null;
+		final long s = System.currentTimeMillis();
+
+		classObj = c.compile("Test", getSource(Integer.toString(0)));
 
 		final Runnable r = RuntimeCompiler.constructInstance(classObj);
 		r.run();
+
+		final long e = System.currentTimeMillis();
+		System.out.println("Took " + (e - s) + " milliseconds");
 	}
 
-	private static String getSource() {
+	private static String getSource(final String text) {
 		return "public class Test implements Runnable {" + //
 				"public void run() {" + //
-				"System.out.println(\"Hello World\");" + //
+				"System.out.println(\" " + text + "\");" + //
 				"}" + //
 				"}";
 	}
--- src/main/java/com/github/sdankbar/jrungen/RuntimeCompiler.java
+++ src/main/java/com/github/sdankbar/jrungen/RuntimeCompiler.java
@@ -1,8 +1,11 @@
 package com.github.sdankbar.jrungen;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import javax.tools.Diagnostic;
@@ -28,8 +31,23 @@ public class RuntimeCompiler {
 		throw new CompilationException(errorMsg);
 	}
 
-	public static <T> T constructInstance(final Class<T> c) throws InstantiationException, IllegalAccessException {
-		return c.newInstance();
+	/**
+	 * Attempts to use the default, no argument constructor for the class
+	 * represented by Class<T> c to construct an instance of that class.
+	 *
+	 * @param <T> Type of the object to return.
+	 * @param c   Class of the class to instantiate. <T> should be an instance of
+	 *            the class or an interface implemented by the class.
+	 * @return The instantiated object.
+	 * @throws CompilationException Thrown if unable to instantiate the object.
+	 */
+	public static <T> T constructInstance(final Class<T> c) throws CompilationException {
+		try {
+			return c.getConstructor().newInstance();
+		} catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException
+				| IllegalArgumentException | InvocationTargetException e) {
+			throw new CompilationException("Error constructing instance of class " + c.getSimpleName(), e);
+		}
 	}
 
 	private final JavaCompiler compilerReference = javax.tools.ToolProvider.getSystemJavaCompiler();
@@ -50,6 +68,43 @@ public class RuntimeCompiler {
 		} catch (final IOException excp) {
 			throw new CompilationException("Error loading compiled class", excp);
 		}
+	}
+
+	public <T, R> Class<Function<T, R>> compileFunctionalInterface(final Class<T> argType, final Class<R> returnType,
+			final String body) throws CompilationException {
+		final DiagnosticCollector<JavaFileObject> collector = new DiagnosticCollector<>();
+
+		final String className = "Func" + UUID.randomUUID().toString().replace("-", "");
+
+		final InMemoryClassFile classOuput = new InMemoryClassFile(className);
+		final StandardJavaFileManager standardFileManager = compilerReference.getStandardFileManager(collector, null,
+				null);
+
+		final String sourceCode = getFunctionalSourceCode(className, argType, returnType, body);
+		try (final JavaFileManager wrappedManager = new InMemoryFileManager(standardFileManager, classOuput)) {
+			compileSynchronous(className, sourceCode, collector, wrappedManager);
+
+			// Load the in memory bytecode as a Class.
+			return loadClass(className, classOuput);
+		} catch (final ClassNotFoundException excp) {
+			throw new CompilationException("Error loading compiled class", excp);
+		} catch (final IOException excp) {
+			throw new CompilationException("Error loading compiled class", excp);
+		}
+	}
+
+	private String getFunctionalSourceCode(final String className, final Class<?> argType, final Class<?> returnType,
+			final String body) {
+		final String argT = argType.getSimpleName();
+		final String retT = returnType.getSimpleName();
+		return "import java.util.function.Function;\n" + //
+				"import " + argType.getName() + ";\n" + //
+				"import " + returnType.getName() + ";\n" + //
+				"public class " + className + " implements Function<" + argT + "," + retT + "> {\n" + //
+				"public " + retT + " apply(" + argT + " arg) {\n" + //
+				body + //
+				"}\n" + //
+				"}\n";
 	}
 
 	private void compileSynchronous(final String className, final String sourceCode,
